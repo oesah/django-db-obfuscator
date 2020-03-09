4850235a7d47b7ed2b031f6c66933ea455d5eff1
oesah <o.sahin@oesah.de>
2020-03-09 19:26:12 +0100

    fix: remove history from setup py

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('CHANGELOG.md') as history_file:
-    history = history_file.read()
-
 requirements = []
 
 setup_requirements = [
@@ -42,7 +39,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='obfuscator',
     name='obfuscator',
@@ -51,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oesah/obfuscator',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
