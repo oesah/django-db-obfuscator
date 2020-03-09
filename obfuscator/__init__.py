a1c23cbb72f9a5b1ab5962ab69de0807eb9a45b2
oesah <o.sahin@oesah.de>
2020-03-09 20:00:56 +0100

    feat: add legacy lib

--- obfuscator/__init__.py
+++ obfuscator/__init__.py
@@ -4,3 +4,5 @@ __author__ = """Oezer Sahin"""
 __email__ = 'os@oesah.ch'
 __version__ = '0.1.6'
 __url__ = 'https://github.com/oesah/django-db-obfuscator/'
+
+default_app_config = 'obfuscator.apps.ObfuscatorConfig'
--- setup.py
+++ setup.py
@@ -3,10 +3,20 @@
 
 from setuptools import setup, find_packages
 
+# Package meta-data.
+VERSION = __import__("obfuscator").__version__
+NAME = 'django-db-obfuscate'
+DESCRIPTION = 'Django app to obfuscate text data.'
+URL = 'https://github.com/oesah/django-db-obfuscator'
+EMAIL = 'os@oesah.ch'
+AUTHOR = 'Oezer Sahin'
+
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-requirements = []
+requirements = [
+    'setuptools-git >= 1.2',
+]
 
 setup_requirements = [
     'pytest-runner',
@@ -17,21 +27,24 @@ test_requirements = [
 ]
 
 setup(
-    author="Oezer Sahin",
-    author_email='os@oesah.ch',
-    python_requires='>=3.5',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires='>=3.6',
     classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.2',
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="Django app to obfuscate text data.",
+    description=DESCRIPTION,
     entry_points={
         'console_scripts': [
             'obfuscator=obfuscator.cli:main',
@@ -42,12 +55,12 @@ setup(
     long_description=readme,
     include_package_data=True,
     keywords='obfuscator',
-    name='django-db-obfuscator',
+    name=NAME,
     packages=find_packages(include=['obfuscator', 'obfuscator.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/oesah/django-db-obfuscator',
-    version='0.1.6',
+    url=URL,
+    version=VERSION,
     zip_safe=False,
 )
