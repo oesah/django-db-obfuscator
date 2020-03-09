0a39c34329e982685371c7e9d5b57d166373ba13
oesah <o.sahin@oesah.de>
2020-03-09 19:42:23 +0100

    chore: revert to original

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# [Changelog](https://github.com/oesah/django-db-obfuscator//releases)
-
-## [0.1.1](https://github.com/oesah/django-db-obfuscator//compare/0.1.1...0.1.1)
-
--   [36f7ebd](https://github.com/oesah/django-db-obfuscator//commit/36f7ebd) fix: change to markdown
--   [9729fc8](https://github.com/oesah/django-db-obfuscator//commit/9729fc8) fix: README formatting errors
--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
+with open('CHANGELOG.md') as history_file:
+    history = history_file.read()
+
 requirements = []
 
 setup_requirements = [
@@ -39,7 +42,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme,
+    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='obfuscator',
     name='django-db-obfuscator',
