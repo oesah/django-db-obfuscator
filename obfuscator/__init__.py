12e95c93a24f5111f3a322f7cb8bab4e4f2e0050
oesah <o.sahin@oesah.de>
2020-03-09 20:54:22 +0100

    Bump version: 0.2.1 → 0.3.0

--- obfuscator/__init__.py
+++ obfuscator/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Oezer Sahin"""
 __email__ = 'os@oesah.ch'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __url__ = 'https://github.com/oesah/django-db-obfuscator/'
 
 default_app_config = 'obfuscator.apps.ObfuscatorConfig'
